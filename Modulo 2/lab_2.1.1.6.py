a8474eac39608b4494e6f697044c326a867a3a8e
Dacko90 <Dacko90@users.noreply.github.com>
2022-09-20 08:49:23 -0500

    Create lab_2.1.1.6.py

---
+++ Modulo 2/lab_2.1.1.6.py
@@ -0,0 +1,7 @@
+#Descripcion: Programa muy facil de la funcion de print
+#Autor: Camarillo Velázquez Diego Apolinar
+#Fecha: 20-sep-2022
+
+print("hola mundo")
+print("Soy Paquito")
+print("Bienvenido a Python")
